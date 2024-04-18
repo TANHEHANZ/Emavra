56191dc22f0993e72a2c2cf2a724f66f0d713c61
Hanz Tapia Choque <hantach10@gmail.com>
2024-04-18 19:37:38 -0400

    Update App.jsx

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,13 +3,24 @@ import { fas } from '@fortawesome/free-solid-svg-icons';
 import { HashRouter, Route, Routes } from "react-router-dom";
 import Layout from "./pages/layout";
 import { dataRoutes } from "./services/routes";
+import { dataRoutesDashboard } from "./services/routes_dashboard";
 import React from "react";
+import Dashboard_layaut from './pages/dashboard/dashboard_layaut';
+import Login from './pages/dashboard/login';
 
 library.add(fas);
 
 function App() {
   const renderRoutes = (dataRender ) => {
     return dataRender.map((ruta) => (
+      <React.Fragment key={ruta.path}>
+        <Route path={ruta.path} element={<ruta.component />} />
+        {ruta.children && renderRoutes(ruta.children)}
+      </React.Fragment>
+    ));
+  };
+  const renderRoutesDashborad = (dataRoutesDashboard) => {
+    return dataRoutesDashboard.map((ruta) => (
       <React.Fragment key={ruta.path}>
         <Route path={ruta.path} element={<ruta.component />} />
         {ruta.children && renderRoutes(ruta.children)}
@@ -23,6 +34,11 @@ function App() {
         <Route path="/" element={<Layout />}>
           {renderRoutes(dataRoutes)}
         </Route>
+      </Routes>
+      <Routes>
+      <Route path="/dashboard" element={<Dashboard_layaut />}>
+      {/* {renderRoutesDashborad(dataRoutesDashboard)} */}
+      </Route>
       </Routes>
     </HashRouter>
   );
